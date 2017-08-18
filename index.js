ea6fbfa17098a1a21c934979e8f2a7981002b5f3
Gerald Emory <geraldemory@Geralds-MacBook-Pro.local>
2017-08-18 09:03:39 -0400

    Fixed up the project to fixed user names

--- index.js
+++ index.js
@@ -14,11 +14,62 @@ app.use(session({
   resave: false,
   saveUninitialized: true
 }));
+const userId =[
+  {
+    'name': 'logan',
+    'passWord': '1234'
+  }
 
-app.get('/', (req,res) =>{
-  res.render('index');
+];
+
+app.use((req,res,next) =>{
+  if(!req.session.loginCheck){
+    req.session.loginCheck = [];
+    req.session.dataName = [];
+  }
+  next();
 });
 
+app.get('/', (req,res) =>{
+  console.log(req.session);
+  if (req.session.loginCheck[0] == 'true') {
+    res.render("index", {dataName: req.session.dataName});
+  }else {
+    res.render('login');
+  }
+
+});
+
+// app.post('/',(req,res) =>{
+//
+//   console.log(req.body);
+//   res.render(req.body);
+// });
+
+app.post('/settingForm',(req,res) =>{
+  let name = req.body.nameLogin;
+  let pass = req.body.passLogin;
+  userId.forEach(function(user){
+    if (user.name === name && user.passWord === pass ) {
+      req.session.loginCheck.push('true');
+      req.session.dataName.push(user.name);
+      console.log(user);
+      res.redirect('/')
+    }else {
+      res.redirect('/');
+    }
+  });
+
+
+  //console.log(req.session.name);
+
+
+});
+
+
+// app.get('/loggedIn', (req,res) =>{
+//   res.render('loggedIn');
+// });
 
 
 app.listen(3000);
